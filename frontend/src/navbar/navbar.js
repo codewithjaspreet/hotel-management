8a09a42dd9a2ebf7f8483d2df11d2e5210cbbfa2
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-03-11 14:18:42 +0530

    fixing

--- frontend/src/navbar/navbar.js
+++ frontend/src/navbar/navbar.js
@@ -5,11 +5,11 @@ const NavBar = () => {
   return (
     <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
       <div class="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="https://flowbite.com/" class="flex items-center">
+        <a href="/" class="flex items-center">
           <img
             src= {logo}
             class="h-6 mr-3 sm:h-9"
-            alt="Flowbite Logo"
+            alt="Logo"
           />
           <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             Hotel Management Admin Panel
@@ -22,20 +22,7 @@ const NavBar = () => {
           aria-controls="navbar-default"
           aria-expanded="false"
         >
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
+          
         </button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
